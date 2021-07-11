6ad7d7845f5e4c7c61926bb0e7c1809b01a43702
Vinod Thube <vinithube@gmail.com>
2021-07-11 15:46:19 +0530

    Initial setup Spring Boot Netflix Ribbon

---
+++ src/main/java/com/example/ribbon/SpringBootRibbonLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRibbonLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRibbonLoadBalancerApplication.class, args);
+	}
+
+}
